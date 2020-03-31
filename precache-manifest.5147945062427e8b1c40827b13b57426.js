5e1ca67aeb886c84b2deb027fae477eb45bedc90
DipenMavani <mavanidipen252@gmail.com>
2020-03-31 17:12:18 +0530

    Updates

---
+++ precache-manifest.5147945062427e8b1c40827b13b57426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8235266a41b58d5471b8561dba9176",
+    "url": "/privatechatapp/index.html"
+  },
+  {
+    "revision": "240fe775e6ea3cf8ed49",
+    "url": "/privatechatapp/static/css/main.5a0f12ef.chunk.css"
+  },
+  {
+    "revision": "7d445f2d86468a2def8e",
+    "url": "/privatechatapp/static/js/2.720dfb5e.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/privatechatapp/static/js/2.720dfb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240fe775e6ea3cf8ed49",
+    "url": "/privatechatapp/static/js/main.60fef4e0.chunk.js"
+  },
+  {
+    "revision": "9571edd436fe574a4440",
+    "url": "/privatechatapp/static/js/runtime-main.9a9eb904.js"
+  }
+]);
